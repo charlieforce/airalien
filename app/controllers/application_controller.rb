08b0b736f14cc75ba33f4e0722b8176e68b82d7c
CharlieKB <charlieforce1999@gmail.com>
2016-12-27 15:12:56 -0800

    made a change on user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_paramters, if: :devise_controller?
 
   protected
-  	def configure_permitted_paramters
-  		devise_parameter_sanitizer.for(:sign_up) << :fullname
-  		devise_parameter_sanitizer.for(:account_update) << :fullname << :phone_number << :description << :email << :password
+  	def configure_permitted_parameters
+  		devise_parameter_sanitizer.permit(:sign_up, keys:[:fullname])
+  		devise_parameter_sanitizer.permit(:account_update, keys:[:fullname, :phone_number, :description, :email, :password])
   	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   devise_for 	:users,
   						:path => '',
-  						:path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
+  						:path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile', :sign_up => 'new'},
   						:controllers => {:omniauth_callbacks => 'omniauth_callbacks',
   														 :registrations => 'registrations'
   														}
