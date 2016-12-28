c3c6b4fbcbb65b3c83fa8d51cdd1a5aa3a1697e7
CharlieKB <charlieforce1999@gmail.com>
2016-12-28 06:31:09 -0800

    Added the room model and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,17 @@ class User < ActiveRecord::Base
 
   validates :fullname, presence: true, length: {maximum: 50}
 
-  def self.from_omniauth(auth)
-    user = User.where(email: auth.info.email).first
+  has_many :rooms
 
-    if user
-      return user
-    else
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-          user.fullname = auth.info.name
-          user.provider = auth.provider
-          user.uid = auth.uid
-          user.email = auth.info.email
-          user.image = auth.info.image
-          user.password = Devise.friendly_token[0,20]
+  def self.from_omniauth(auth)
+  	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.fullname = auth.info.name
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.image = auth.info.image
+        user.password = Devise.friendly_token[0,20]
       end
-    end
   end
 
 end
